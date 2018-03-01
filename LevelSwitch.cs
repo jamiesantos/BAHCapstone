8349c0968474a07dedc23675db746b5598c7f98e
jamiesantos <jamiesanto@gmail.com>
2018-03-01 02:51:08 -0500

    Uploading Unity files
    😄

---
+++ LevelSwitch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSwitch : MonoBehaviour {
+
+	public string CabinInside;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			SceneManager.LoadScene(CabinInside);
+	    }
+    }
+}
